b8f2d75d60d35db1a0d21ed1e5d7a1a111877da4
zaenalrfn <ZAOfficial24@gmail.com>
2024-08-24 08:04:20 +0700

    register auth service (redux toolkit)

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import {setupListeners} from "@reduxjs/toolkit/dist/query"
+import {authApi} from "@/services/auth"
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    [authApi.reducerPath]: authApi.reducer
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat([authApi.middleware])
 })
 
 setupListeners(store.dispatch)
