e72930145160040571ca03b75030d40f98276968
zaenalrfn <ZAOfficial24@gmail.com>
2024-08-24 07:57:38 +0700

    request and response type (redux toolkit)

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,20 +1,31 @@
-import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/dist/query/react"
+import BaseResponse from "@/types/response";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/dist/query/react";
+
+interface AuthResponse extends BaseResponse {
+  data: {};
+}
+
+type UserAuthForm = {
+  name: string;
+  email: string;
+  password: string;
+};
 
 export const authApi = createApi({
-    reducerPath: "authApi",
-    baseQuery: fetchBaseQuery({
-        baseUrl: "/api",
+  reducerPath: "authApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "/api",
+  }),
+  tagTypes: ["auth"],
+  endpoints: (builder) => ({
+    register: builder.mutation<AuthResponse, any>({
+      query: (body) => ({
+        url: "/auth/sign-up",
+        method: "POST",
+        body,
+      }),
     }),
-    tagTypes: ['auth'],
-    endpoints: (builder) => ({
-        register: builder.mutation<any, any>({
-            query: (body) => ({
-                url: "/auth/sign-up",
-                method: "POST",
-                body
-            })
-        })
-    })
-})
+  }),
+});
 
-export const {useRegisterMutation} = authApi
+export const { useRegisterMutation } = authApi;
