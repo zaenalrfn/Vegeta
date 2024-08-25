4ea1492e445e1952947c314c8ed2d80b63931f0a
zaenalrfn <ZAOfficial24@gmail.com>
2024-08-25 17:21:59 +0700

    create products endpoint

---
+++ src/app/api/product/route.ts
@@ -0,0 +1,18 @@
+import Response from "@/lib/api.response";
+import { prisma } from "@/lib/prisma";
+
+export async function GET() {
+    try {
+        const products = await prisma.product.findMany({})
+        return Response({
+            message: "Get all products",
+            data: products,
+        })    
+    } catch (error: any) {
+      return Response({
+        message: "Failed to get product",
+        data: error,
+        status: 500,
+      })  
+    }
+}
