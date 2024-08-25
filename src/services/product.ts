b3ab150b925d9dd75eb99fceef112efbb96909f0
zaenalrfn <ZAOfficial24@gmail.com>
2024-08-25 19:35:08 +0700

    product response interface (home page)

--- src/services/product.ts
+++ src/services/product.ts
@@ -1,4 +1,10 @@
+import BaseResponse from "@/types/response";
+import { Product } from "@prisma/client";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/dist/query/react";
+
+interface ProductResponse extends BaseResponse {
+    data: Product[],
+}
 
 export const productApi = createApi({
     reducerPath: "productApi",
@@ -7,7 +13,7 @@ export const productApi = createApi({
     }),
     tagTypes: ["product"],
     endpoints: (builder) => ({
-        getAllProducts: builder.query<any, any>({
+        getAllProducts: builder.query<ProductResponse, any>({
             query: () => ({
                 url: "/"
             })
