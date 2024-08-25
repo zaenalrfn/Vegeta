64273aa5dc122f84c428d3f4bb108ca9b8bf89bf
zaenalrfn <ZAOfficial24@gmail.com>
2024-08-25 20:56:25 +0700

    add skeleton loading (home page)

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import Image from "next/legacy/image";
 import Link from "next/link";
 
@@ -15,8 +15,7 @@ import ImageBanner from "@/assets/images/image-banner.png";
 import { useGetAllProductsQuery } from "@/services/product";
 
 export default function Home() {
-
-  const {data} = useGetAllProductsQuery({})
+  const { data, isLoading } = useGetAllProductsQuery({});
 
   return (
     <main className="flex flex-col w-full min-h-screen items-center pb-8">
@@ -31,23 +30,16 @@ export default function Home() {
 
         <div className="flex flex-col">
           <div className="flex justify-between mb-6">
-            <div className="text-leaf text-3xl font-semibold">
-              Produk Unggulan
-            </div>
-            <Link
-              href="/product"
-              className="text-base p-0 h-auto bg-white text-neutral-600"
-            >
+            <div className="text-leaf text-3xl font-semibold">Produk Unggulan</div>
+            <Link href="/product" className="text-base p-0 h-auto bg-white text-neutral-600">
               Lihat Selengkapnya {">"}
             </Link>
           </div>
-          <ProductShowcase gridConfig={"grid-cols-4"} products={data?.data?.slice(0, 4) || [] } />
+          <ProductShowcase gridConfig={"grid-cols-4"} products={data?.data?.slice(0, 4) || []} isLoading={isLoading} />
         </div>
 
         <div className="mt-8">
-          <div className="text-leaf text-3xl font-semibold">
-            Produk berdasarkan kategori
-          </div>
+          <div className="text-leaf text-3xl font-semibold">Produk berdasarkan kategori</div>
 
           <div className="mt-4 grid grid-cols-3 gap-6">
             {ProductCategoryJSON.map((category) => (
@@ -66,28 +58,20 @@ export default function Home() {
           <div className="text-leaf text-3xl font-semibold">Tentang Kami</div>
           <div className="flex flex-col mt-6 gap-8 text-gray-400">
             <p>
-              Selamat datang di Vegeta, destinasi terbaik untuk aneka sayuran
-              dan buah segar siap kirim ke seluruh Indonesia! Kami dengan bangga
-              mempersembahkan diri sebagai tim yang berdedikasi untuk memberikan
-              kualitas terbaik dan pilihan yang beragam untuk kebutuhan sayuran
-              dan buah segar Anda.
+              Selamat datang di Vegeta, destinasi terbaik untuk aneka sayuran dan buah segar siap kirim ke seluruh Indonesia! Kami dengan bangga
+              mempersembahkan diri sebagai tim yang berdedikasi untuk memberikan kualitas terbaik dan pilihan yang beragam untuk kebutuhan sayuran dan
+              buah segar Anda.
             </p>
             <p>
-              Kami berusaha untuk selalu memberikan pelayanan terbaik kepada
-              pelanggan kami. Tim kami selalu siap membantu Anda dalam memilih
-              aneka sayuran dan buah yang sesuai dengan kebutuhan dan preferensi
-              Anda. Dengan sistem pengiriman yang efisien, produk-produk segar
-              dari Vegeta akan tiba di depan pintu Anda dalam kondisi prima,
-              siap untuk melengkapi hidangan sehat dan lezat Anda.
+              Kami berusaha untuk selalu memberikan pelayanan terbaik kepada pelanggan kami. Tim kami selalu siap membantu Anda dalam memilih aneka
+              sayuran dan buah yang sesuai dengan kebutuhan dan preferensi Anda. Dengan sistem pengiriman yang efisien, produk-produk segar dari
+              Vegeta akan tiba di depan pintu Anda dalam kondisi prima, siap untuk melengkapi hidangan sehat dan lezat Anda.
             </p>
             <p>
-              Terima kasih atas kepercayaan Anda pada kami. Kami berharap Anda
-              menikmati pengalaman berbelanja di situs kami dan menemukan
-              produk-produk berkualitas tinggi yang akan melengkapi kehidupan
-              sehat dan bahagia Anda. Jangan ragu untuk menghubungi tim kami
-              jika Anda memerlukan bantuan atau memiliki pertanyaan. Bersama,
-              mari kita jaga kesehatan dan kelezatan hidup dengan produk-produk
-              berkualitas dari kami.
+              Terima kasih atas kepercayaan Anda pada kami. Kami berharap Anda menikmati pengalaman berbelanja di situs kami dan menemukan
+              produk-produk berkualitas tinggi yang akan melengkapi kehidupan sehat dan bahagia Anda. Jangan ragu untuk menghubungi tim kami jika Anda
+              memerlukan bantuan atau memiliki pertanyaan. Bersama, mari kita jaga kesehatan dan kelezatan hidup dengan produk-produk berkualitas dari
+              kami.
             </p>
           </div>
         </div>
