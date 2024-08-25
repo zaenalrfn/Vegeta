638461a9fb0f24a1a46d1d87b273af67b86d98fa
zaenalrfn <ZAOfficial24@gmail.com>
2024-08-25 21:24:40 +0700

    pagination endpoint (products)

--- src/app/api/product/route.ts
+++ src/app/api/product/route.ts
@@ -1,18 +1,30 @@
 import Response from "@/lib/api.response";
 import { prisma } from "@/lib/prisma";
+import { NextRequest } from "next/server";
 
-export async function GET() {
-    try {
-        const products = await prisma.product.findMany({})
-        return Response({
-            message: "Get all products",
-            data: products,
-        })    
-    } catch (error: any) {
-      return Response({
-        message: "Failed to get product",
-        data: error,
-        status: 500,
-      })  
-    }
+export async function GET(req: NextRequest) {
+  try {
+    const take = 9;
+    const query = req.nextUrl.searchParams;
+    const page = query.get("page") ? parseInt(query.get("page") as string) - 1 : 0;
+    const skip = page * take;
+    const totalProducts = await prisma.product.count({})
+    const products = await prisma.product.findMany({
+      take,
+      skip,
+    });
+    return Response({
+      message: "Get all products",
+      data: {
+        total: totalProducts,
+        data: products,
+      },
+    });
+  } catch (error: any) {
+    return Response({
+      message: "Failed to get product",
+      data: error,
+      status: 500,
+    });
+  }
 }
